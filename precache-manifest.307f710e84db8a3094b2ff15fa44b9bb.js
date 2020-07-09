b842a98e79da03a0fbf03444bb912790bbca2c37
alpoliakov <alpoliakov73@gmail.com>
2020-07-09 14:24:01 +0300

    Updates

---
+++ precache-manifest.307f710e84db8a3094b2ff15fa44b9bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8343bc3275e838804fb864360b60fd1c",
+    "url": "/movies_app/index.html"
+  },
+  {
+    "revision": "d34b92aa1f081841fd9a",
+    "url": "/movies_app/static/css/main.a3fc7239.chunk.css"
+  },
+  {
+    "revision": "1b32bd76b237bfc08813",
+    "url": "/movies_app/static/js/2.6a6d182a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movies_app/static/js/2.6a6d182a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d34b92aa1f081841fd9a",
+    "url": "/movies_app/static/js/main.59fbc73d.chunk.js"
+  },
+  {
+    "revision": "4d44904df4d3735bc5ff",
+    "url": "/movies_app/static/js/runtime-main.a1e4a797.js"
+  }
+]);
